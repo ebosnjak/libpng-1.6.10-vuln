2056507be30f7c01a2804a09ee82365bbb245ef8
Eugen Bosnjak <eugen.bosnjak@epfl.ch>
2025-05-13 20:46:37 +0200

    aaaaaaaaaaaaa

--- contrib/oss-fuzz/libpng_read_fuzzer.cc
+++ contrib/oss-fuzz/libpng_read_fuzzer.cc
@@ -147,10 +147,10 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   png_set_read_fn(png_handler.png_ptr, png_handler.buf_state, user_read_data);
   png_set_sig_bytes(png_handler.png_ptr, kPngHeaderSize);
 
-  if (setjmp(png_jmpbuf(png_handler.png_ptr))) {
+  /*if (setjmp(png_jmpbuf(png_handler.png_ptr))) {
     PNG_CLEANUP
     return 0;
-  }
+  }*/
 
   // Reading.
   png_read_info(png_handler.png_ptr, png_handler.info_ptr);
